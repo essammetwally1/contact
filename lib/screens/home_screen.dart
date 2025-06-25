c6c4f49d77fc4d4cf65e0ef7d2bbc6cb904567df
essammetwally1 <essammetwally11@gmail.com>
2025-06-25 03:57:28 +0300

    home screen done

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -7,6 +7,42 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(body: Center(child: Text('Home Screen')));
+    return Scaffold(
+      backgroundColor: Color(0xff29384D),
+
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(left: 18, top: 8),
+              child: Image.asset('assets/route.png', height: 39, width: 117),
+            ),
+            Expanded(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(left: 18, top: 8),
+                    child: Image.asset('assets/homeimage.png'),
+                  ),
+                  SizedBox(
+                    height: MediaQuery.sizeOf(context).height * .1,
+                    child: Text(
+                      'There is No Contacts Added Here',
+                      style: TextStyle(
+                        color: Color(0xffFFF1D4),
+                        fontSize: 20,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
